022da3a76fca70e23f1227569dbf084c9c3f88cf
EsraaMohamedElGamal <132311342+EsraaMohamedElGamal@users.noreply.github.com>
2023-05-04 16:39:13 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,8 @@
+  
+ 
+   function msg(){  
+                var v= prompt("your rating of our website and services out of 5");  
+                alert("thank you");  
+  
+                   } 
+ 
